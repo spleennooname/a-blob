695f410199e5432e8ce1ce8a28505e66a614fd9a
spleennooname <spleen666@gmail.com>
2019-04-05 22:48:03 +0200

    add context lost/restore logic

--- src/glsl/vert.glsl
+++ src/glsl/vert.glsl
@@ -1,3 +1,5 @@
+precision highp float;
+
 attribute vec3 position;
 
 void main() {
--- src/js/demo.js
+++ src/js/demo.js
@@ -1,5 +1,4 @@
 import '../scss/styles.scss';
-
 import { getGPUTier } from 'detect-gpu';
 
 'use strict';
@@ -21,10 +20,10 @@ let canvas, gl,
   screenHeight = 320,
   stats,
   now = 0,
-  then = 0;
+  then = 0,
 
-let fps = 60;
-let interval = 1000 / fps;
+  fps = 60,
+  interval = 1000 / fps;
 
 function getBestPixelRatio() {
   return GPUTier.tier.indexOf("MOBILE_TIER_0") !== -1 || GPUTier.tier.indexOf("MOBILE_TIER_1") !== -1 && window.devicePixelRatio > 1 ? 1 : window.devicePixelRatio;
@@ -34,23 +33,10 @@ function getBestFPS() {
   return GPUTier.tier.indexOf('MOBILE_TIER_0') !== -1 && window.devicePixelRatio >= 1 ? 50 : 60;
 }
 
-function init() {
-
-  GPUTier = getGPUTier({
-    mobileBenchmarkPercentages: [10, 40, 30, 20], // (Default) [TIER_0, TIER_1, TIER_2, TIER_3]
-    desktopBenchmarkPercentages: [10, 40, 30, 20], // (Default) [TIER_0, TIER_1, TIER_2, TIER_3]
-  });
-
-  //console.log(GPUTier);
-  const log = 'devicePixelRatio=' + window.devicePixelRatio + ", applied: "+getBestPixelRatio()+" tier=" + GPUTier.tier + " type=" + GPUTier.type;
-
-  document.querySelector('.log').innerHTML = log;
-
-  realToCSSPixels = getBestPixelRatio();
-  fps = getBestFPS();
-  fps = interval / 1000;
+function demo() {
 
   canvas = document.querySelector('#canvas');
+
   try {
     gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
   } catch (err) {
@@ -63,13 +49,42 @@ function init() {
     return;
   }
 
+  canvas.addEventListener('webglcontextlost', event => {
+    console.warn('lost');
+    event.preventDefault();
+    stop();
+  }, false);
+
+  canvas.addEventListener('webglcontextrestored', event => { console.warn('restored'); init(); }, false);
+
+  init();
+
+  //gl.getExtension('WEBGL_lose_context').restoreContext();
+  //gl.getExtension('WEBGL_lose_context').loseContext();
+}
+
+function init() {
+
+  GPUTier = getGPUTier({
+     mobileBenchmarkPercentages: [10, 40, 30, 20], // (Default) [TIER_0, TIER_1, TIER_2, TIER_3]
+     desktopBenchmarkPercentages: [10, 40, 30, 20], // (Default) [TIER_0, TIER_1, TIER_2, TIER_3]
+   });
+
+   //console.log(GPUTier);
+  const log = 'devicePixelRatio=' + window.devicePixelRatio + ', applied: ' + getBestPixelRatio() + ' tier=' + GPUTier.tier + ' type=' + GPUTier.type;
+
+  document.querySelector('.log').innerHTML = log;
+
+  realToCSSPixels = getBestPixelRatio();
+  fps = getBestFPS();
+  fps = interval / 1000;
+
   stats = new Stats();
   stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
   document.body.appendChild(stats.domElement);
 
   // create a buffer with single clipspace
   positionBuffer = gl.createBuffer();
-
   // Bind the position buffer.
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(
@@ -84,7 +99,6 @@ function init() {
     ]),*/
     // quad
     //new Float32Array([-1, -1, -1, 1, 1, -1, 1, 1]),
-
     new Float32Array([-1, -1, -1, 4, 4, -1]),
     gl.STATIC_DRAW
   );
@@ -112,39 +126,33 @@ function init() {
 
   // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
   gl.vertexAttribPointer(vertexPosition, 2, gl.FLOAT, false, 0, 0);
+
+  start();
 }
-
-
 
 function createProgram(vertex, fragment) {
   const program = gl.createProgram();
-  const vs = createShader(vertex, gl.VERTEX_SHADER);
-  const fs= createShader(fragment, gl.FRAGMENT_SHADER);
 
-  if (vs === null || fs === null) {
-    return null;
-  }
-
-  gl.attachShader(program, vs);
+  const fs = createShader(fragment, gl.FRAGMENT_SHADER);
   gl.attachShader(program, fs);
 
-  gl.linkProgram(program);
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (!success) {
-    console.warn('createProgram error');
-  }
-  gl.deleteShader(vs);
-  gl.deleteShader(fs);
+  const vs = createShader(vertex, gl.VERTEX_SHADER);
+  gl.attachShader(program, vs);
 
-  return success ? program : null;
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS) && !gl.isContextLost() ) {
+    console.warn('createProgram error');
+    return null;
+  }
+  return program;
 }
 
 function createShader(src, type) {
   const shader = gl.createShader(type);
+
   gl.shaderSource(shader, src);
   gl.compileShader(shader);
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
-  if (!success) {
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS) && !gl.isContextLost()) {
     console.warn('createShader error', gl.getShaderInfoLog(shader));
     return null;
   }
@@ -152,19 +160,16 @@ function createShader(src, type) {
 }
 
 function resize() {
-
  // Lookup the size the browser is displaying the canvas in CSS pixels
   // and compute a size needed to make our drawingbuffer match it in
   // device pixels.
   var displayWidth  = Math.floor(canvas.clientWidth  * realToCSSPixels);
   var displayHeight = Math.floor(canvas.clientHeight * realToCSSPixels);
-
   // Check if the canvas is not the same size.
   if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
      // Make the canvas the same size
     canvas.width = displayWidth;
     canvas.height = displayHeight;
-
     screenWidth = canvas.width;
     screenHeight = canvas.height;
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
@@ -186,13 +191,11 @@ function animate() {
 }
 
 function render(time) {
-
   // Clear the canvas AND the depth buffer.
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
+  // set uniforms
   gl.uniform1f(uTime, time);
   gl.uniform2f(uResolution,screenWidth, screenHeight);
-
   //gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
@@ -214,6 +217,4 @@ function destroy() {
 }
 
 //
-init();
-
-start();
+demo();
